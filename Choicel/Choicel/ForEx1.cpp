910a48733f3e0c565f81fdcd5564e6bbdc74ecc9
kangtaeyeonn <ksj1350@nate.com>
2025-03-23 11:16:26 +0900

    c++ 소스

---
+++ Choicel/Choicel/ForEx1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+/*
+	for 반복문
+	for(초기값; 종료값; 증감값)
+	{
+		실행문
+	}
+*/
+
+int main()
+{
+	// 1 ~ 10까지 출력
+	for (int n = 1; n <= 10; n++)
+	{
+		cout << n << " ";
+	}
+	cout << "\n=================================\n";
+
+	// 1 ~ 10까지 합계 출력
+	int sum = 0;
+	for (int n = 1; n <= 10; n++)
+	{
+		sum += n;
+		cout << "n=" << n << ", sum= " << sum << endl;
+	}
+	cout << endl;
+	cout << "합계 " << sum << endl;
+
+
+
+	return 0;
+}
