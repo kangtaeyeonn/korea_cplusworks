4ac24480639318ba8d2850e242a13037c1808239
kangtaeyeonn <ksj1350@nate.com>
2025-03-29 11:52:10 +0900

    funcion, pointer

---
+++ Functions/Functions/VoidFunction.cpp
@@ -0,0 +1,27 @@
+//#include <iostream>
+//#include <string> //string자료형 사용
+//using namespace std;
+//
+//
+///*
+//	반환 자료형이 없는 함수(void형)
+//*/
+//void sayHello()
+//{
+//	cout << "안녕하세요\n";
+//}
+//void sayHello2(string name)
+//{
+//	cout << name << "안녕하세요\n";
+//}
+//
+//int main()
+//{
+//	//인사하는 함수 호출
+//	sayHello();
+//
+//	sayHello2("유빈");
+//	sayHello2("이정후");
+//
+//	return 0;
+//}
